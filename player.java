6e3f50d879aded577de5483f2e45156ce697511f
Kellie Olsen <kellienstreet@comcast.net>
2017-04-26 21:57:09 -0500

    Add files via upload

---
+++ player.java
@@ -0,0 +1,26 @@
+
+public class player {
+	String name;
+	String Position;
+	int Tackles;
+	int PassingYards;
+	int RushingYards;
+	int Sacks;
+	int ReceivingYards;
+	int Interceptions;
+	int Touchdowns;
+	
+	//public void setName(String n) {
+	//	name = n;
+	//}
+	//public String getName() {
+	//	return name;
+	//}
+	//public void setPosition(String p) {
+	//	Position = p;
+	//}
+	//public String getPosition() {
+		//return Position;
+	//}
+
+}
